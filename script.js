4314bd2c4b83a980f6624d9d6fc7ea027ea1d646
Bartek Dobrzanski <baarti02@gmail.com>
2024-10-08 18:41:36 +0200

    Main js program done and little interface

---
+++ script.js
@@ -0,0 +1,50 @@
+let options = ["rock", "paper", "scissors"];
+
+let pointsUser = 0;
+let pointsComputer = 0;
+
+function getHumanChoice() {
+    let incorrect = true;
+    let index;
+    while (incorrect) {
+        let choice = prompt("Pick your gesture?");
+        if ((index = options.indexOf(choice)) !== -1) {
+            incorrect = false;
+        }
+    }
+    return index;
+}
+
+function generateChoice() {
+    const max = 3;
+    return Math.floor(Math.random() * max);
+}
+
+let userChoice;
+let computerChoice;
+while (pointsUser !== 3 && pointsComputer !== 3) {
+    // userChoice = getHumanChoice() + 1;
+    computerChoice = generateChoice() + 1;
+
+    if (userChoice === 1 || computerChoice === 1) {
+        userChoice %= 3;
+        computerChoice %= 3;
+    }
+
+    if (userChoice === computerChoice) {
+        console.log(`Draw!`);
+    } else if (userChoice < computerChoice) {
+        pointsUser++;
+        console.log(`User wins`);
+    } else {
+        pointsComputer++;
+        console.log(`Computer wins`);
+    }
+}
+
+if (pointsUser === 3) {
+    console.log(`User wins the game!`)
+}
+if (pointsComputer === 3) {
+    console.log(`Computer wins the game!`);
+}
