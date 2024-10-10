d6aec49aca2dbc5ecbd523d1cfd4e73838e1d752
Bartek <baarti02@gmail.com>
2024-10-10 17:27:15 +0200

    Linked javascript with html and css. Made another button. Changed some colors and buttons size. Deleted unused graphics.

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-document.getElementById("button-paragraph").addEventListener("click", myFunction)
+document.getElementById("button-paragraph1").addEventListener("click", () => buttonClicked(0));
+document.getElementById("button-paragraph2").addEventListener("click", () => buttonClicked(1));
+document.getElementById("button-paragraph3").addEventListener("click", () => buttonClicked(2));
+document.getElementById("button-reset").addEventListener("click", () => resetGame());
 
-let options = ["rock", "paper", "scissors"];
+
+let options = ["🖐️", "✊", "✌️"];
 
 let pointsUser = 0;
 let pointsComputer = 0;
 
-function myFunction() {
-    document.getElementById("text-current-action").innerHTML = "Ta funkcja naprawdę działa!"
-
-}
-
-function getHumanChoice() {
-    let incorrect = true;
-    let index;
-    while (incorrect) {
-        let choice = prompt("Pick your gesture?");
-        if ((index = options.indexOf(choice)) !== -1) {
-            incorrect = false;
-        }
-    }
-    return index;
+function resetGame() {
+    pointsUser = 0;
+    pointsComputer = 0;
+    document.getElementById("text-current-action").style.color = "#f07167";
+    document.getElementById("score-player").innerHTML = pointsUser.toString();
+    document.getElementById("score-computer").innerHTML = pointsComputer.toString();
+    document.getElementById("text-current-action").innerHTML = "Let's start!";
+    document.getElementById("left-choice-emoji").innerHTML = "❔️";
+    document.getElementById("right-choice-emoji").innerHTML = "❔️";
 }
 
 function generateChoice() {
@@ -27,11 +25,16 @@ function generateChoice() {
     return Math.floor(Math.random() * max);
 }
 
-let userChoice;
-let computerChoice;
-while (pointsUser !== 3 && pointsComputer !== 3) {
-    // userChoice = getHumanChoice() + 1;
-    computerChoice = generateChoice() + 1;
+function buttonClicked(choice) {
+    let userChoice = choice + 1;
+    let computerChoice = generateChoice() + 1;
+
+    if (pointsUser === 4 || pointsComputer === 4) {
+        resetGame();
+    }
+
+    document.getElementById("left-choice-emoji").innerHTML = options[userChoice-1];
+    document.getElementById("right-choice-emoji").innerHTML = options[computerChoice-1];
 
     if (userChoice === 1 || computerChoice === 1) {
         userChoice %= 3;
@@ -39,19 +42,29 @@ while (pointsUser !== 3 && pointsComputer !== 3) {
     }
 
     if (userChoice === computerChoice) {
-        console.log(`Draw!`);
+        document.getElementById("text-current-action").style.color = "orange";
+        document.getElementById("text-current-action").innerHTML = "Draw!";
     } else if (userChoice < computerChoice) {
         pointsUser++;
-        console.log(`User wins`);
+        document.getElementById("text-current-action").style.color = "#80ed99";
+        document.getElementById("score-player").innerHTML = pointsUser.toString();
+        document.getElementById("text-current-action").innerHTML = "Player wins a point!";
     } else {
         pointsComputer++;
-        console.log(`Computer wins`);
+        document.getElementById("text-current-action").style.color = "#FF292E";
+        document.getElementById("score-computer").innerHTML = pointsComputer.toString();
+        document.getElementById("text-current-action").innerHTML = "Computer wins a point!";
     }
-}
 
-if (pointsUser === 3) {
-    console.log(`User wins the game!`)
-}
-if (pointsComputer === 3) {
-    console.log(`Computer wins the game!`);
+
+    if (pointsUser === 3) {
+        document.getElementById("text-current-action").style.color = "#80ed99";
+        document.getElementById("text-current-action").innerHTML = "Player wins the game!";
+        pointsUser++;
+    }
+    if (pointsComputer === 3) {
+        document.getElementById("text-current-action").style.color = "#FF292E";
+        document.getElementById("text-current-action").innerHTML = "Computer wins the game!";
+        pointsComputer++;
+    }
 }
